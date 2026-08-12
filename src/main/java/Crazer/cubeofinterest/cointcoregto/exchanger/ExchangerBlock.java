d88b58c52b852bb214995e73068dadd5d887d38d
Crazerium <artemlistopad21361@mail.ru>
2026-08-12 21:22:51 +1000

    Add exchanger block variants

--- src/main/java/Crazer/cubeofinterest/cointcoregto/exchanger/ExchangerBlock.java
+++ src/main/java/Crazer/cubeofinterest/cointcoregto/exchanger/ExchangerBlock.java
@@ -2,6 +2,7 @@ package Crazer.cubeofinterest.cointcoregto.exchanger;
 
 import Crazer.cubeofinterest.cointcoregto.currency.CurrencyConfig;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
@@ -9,18 +10,51 @@ import net.minecraft.world.MenuProvider;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Mirror;
 import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraftforge.network.NetworkHooks;
 import org.jetbrains.annotations.Nullable;
 
 public class ExchangerBlock extends BaseEntityBlock {
+    public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
     public ExchangerBlock(Properties properties) {
         super(properties);
+        this.registerDefaultState(
+                this.stateDefinition.any().setValue(FACING, Direction.NORTH)
+        );
+    }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState()
+                .setValue(FACING, context.getHorizontalDirection().getOpposite());
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    @Override
+    public BlockState rotate(BlockState state, Rotation rotation) {
+        return state.setValue(FACING, rotation.rotate(state.getValue(FACING)));
+    }
+
+    @Override
+    public BlockState mirror(BlockState state, Mirror mirror) {
+        return state.rotate(mirror.getRotation(state.getValue(FACING)));
     }
 
     @Nullable
@@ -97,8 +131,14 @@ public class ExchangerBlock extends BaseEntityBlock {
         if (!oldState.is(newState.getBlock())) {
             BlockEntity blockEntity = level.getBlockEntity(pos);
             if (blockEntity instanceof ExchangerBlockEntity exchanger) {
-                exchanger.getItems().setStackInSlot(ExchangerBlockEntity.SLOT_PRODUCT, ItemStack.EMPTY);
-                exchanger.getItems().setStackInSlot(ExchangerBlockEntity.SLOT_PRICE, ItemStack.EMPTY);
+                exchanger.getItems().setStackInSlot(
+                        ExchangerBlockEntity.SLOT_PRODUCT,
+                        ItemStack.EMPTY
+                );
+                exchanger.getItems().setStackInSlot(
+                        ExchangerBlockEntity.SLOT_PRICE,
+                        ItemStack.EMPTY
+                );
                 exchanger.dropContents();
             }
             super.onRemove(oldState, level, pos, newState, moving);
